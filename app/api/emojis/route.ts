eb50b7fa500a86561f3d00cf1c249d48edfe9dd5
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2023-11-08 23:59:18 +0100

    Fix pour empecher une route d'API d'être rendu static

--- app/api/emojis/route.ts
+++ app/api/emojis/route.ts
@@ -3,6 +3,9 @@ import fs from "node:fs";
 import path from "node:path";
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const number = searchParams.get("number");
+
   const emojisPath = path.join(process.cwd(), "public", "emojis");
 
   const emojis = fs.readdirSync(emojisPath);
