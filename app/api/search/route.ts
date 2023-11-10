8ec8eac1c04a5f257ba87d5831e1b2d9f18dd1f6
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2023-11-10 09:16:18 +0100

    Mise à jour route token en dynamic force

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from "next/server";
+import type { RequestInit } from "next/dist/server/web/spec-extension/request";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("q");
   const token = searchParams.get("token");
 
-  var searchReqParams = {
+  var searchReqParams: RequestInit = {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
+    cache: "no-store",
   };
 
   const searchResponse = await fetch(
